e5f06a8b5392aa4574b2c176a2e104fdf585611b
grinyuk <grinyuk309@gmail.com>
2024-03-25 23:54:29 +0200

    added figure movement and cell highlighting

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,13 +1,22 @@
 import React, {FC} from 'react';
 import {Cell} from "../models/Cell";
+import {Simulate} from "react-dom/test-utils";
+import click = Simulate.click;
 
 interface CellProps {
     cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
-        <div className={["cell", cell.color].join(' ')}>
+        <div
+            className={["cell", cell.color, selected ? "selected" : ''].join(' ')}
+            onClick={() => click(cell)}
+            style={{background: cell.available && cell.figure ? "green" : ''}}
+        >
+            {cell.available && !cell.figure && <div className="available"></div>}
             {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
         </div>
     );
--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -10,4 +10,11 @@ export class King extends Figure {
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.KING;
     }
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target))
+            return false;
+        const dx = Math.abs(this.cell.x - target.x);
+        const dy = Math.abs(this.cell.y - target.y);
+        return dx <= 1 && dy <= 1;
+    }
 }
